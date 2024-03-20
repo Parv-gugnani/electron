2f0ea0a9f812bdb3adcab76aadcee1d4657d92ef
Parv-gugnani <9765.parv@gmail.com>
2024-03-20 15:12:11 +0530

    yarn fix

---
+++ script/yarn.js
@@ -0,0 +1,38 @@
+// Importing required modules
+const cp = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+// Extracting Yarn version from the DEPS file
+const YARN_VERSION_MATCH = /'yarn_version': '(.+?)'/.exec(
+  fs.readFileSync(path.resolve(__dirname, "../DEPS"), "utf8")
+);
+if (!YARN_VERSION_MATCH) {
+  console.error("Failed to extract Yarn version from DEPS file");
+  process.exit(1);
+}
+const YARN_VERSION = YARN_VERSION_MATCH[1];
+
+// Determining the correct NPX command based on the platform
+const NPX_CMD = process.platform === "win32" ? "npx.cmd" : "npx";
+
+// If this file is executed directly, spawn a child process to run Yarn with the extracted version
+if (require.main === module) {
+  const child = cp.spawn(
+    NPX_CMD,
+    [`yarn@${YARN_VERSION}`, ...process.argv.slice(2)],
+    {
+      stdio: "inherit",
+      env: {
+        ...process.env,
+        npm_config_yes: "true",
+      },
+    }
+  );
+
+  // Forward the exit code of the child process to the parent process
+  child.on("exit", (code) => process.exit(code));
+}
+
+// Export the extracted Yarn version for external use
+exports.YARN_VERSION = YARN_VERSION;
